ac98f7928845fe69fff04a6d21a50f98ff04c445
Sotatek-DatNghiem <dat.nghiem@sotatek.com>
2026-01-08 15:21:28 +0700

    chore: remove compiled files from src/ and update .gitignore

--- src/validators/product.validator.d.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare class ProductValidator {
-    static validateCreateProduct(req: Request, res: Response, next: NextFunction): Response<any, Record<string, any>> | undefined;
-    static validateUpdateProduct(req: Request, res: Response, next: NextFunction): Response<any, Record<string, any>> | undefined;
-}
-//# sourceMappingURL=product.validator.d.ts.map
